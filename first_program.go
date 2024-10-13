0a173adc364b1095d2083dd18bcdd28d807d0973
Vibhanshu Singh <135852806+singhvibhanshu@users.noreply.github.com>
2024-10-13 10:57:47 +0530

    first program added

---
+++ first_program.go
@@ -0,0 +1,9 @@
+package main // default package
+
+import "fmt" // standard module
+
+// the above first two lines are mandatory in the code
+
+func main(){ // entry point of the program
+	fmt.Println("Hey there! My name is Vibhanshu Singh and I am here to learn GO Programming Language.")
+}
